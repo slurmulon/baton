7af464f43e2e0b39aa5790df63c074f85590a790
slurmulon <anon@blaqkmail.com>
2015-09-17 18:29:21 -0700

    :sparkles: token via query, general cleanup

--- app.js
+++ app.js
@@ -2,20 +2,11 @@
 
 import express      from 'express'
 import path         from 'path'
-import favicon      from 'serve-favicon'
-// import logger       from 'morgan'
 import cookieParser from 'cookie-parser'
 import bodyParser   from 'body-parser'
 
 import * as baton from './lib/src/baton'
 import * as slack from './lib/src/slack'
-
-//   ____             __ _       
-//  / ___|___  _ __  / _(_) __ _ 
-// | |   / _ \| '_ \| |_| |/ _` |
-// | |__| (_) | | | |  _| | (_| |
-//  \____\___/|_| |_|_| |_|\__, |
-//                         |___/
  
 const app    = express()
 const port   = process.env.PORT || 3000
@@ -24,20 +15,12 @@ const router = express.Router()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-// app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-//  ____             _            
-// |  _ \ ___  _   _| |_ ___  ___ 
-// | |_) / _ \| | | | __/ _ \/ __|
-// |  _ < (_) | |_| | ||  __/\__ \
-// |_| \_\___/ \__,_|\__\___||___/
-//
-
-// slack middleware (determines if request is meant for slack with ?slack)
+// slack middleware. determines if request is made from slack-like client via query param
 app.use((req, res, next) => {
   if ('slack' in req.query) {
     req.body = slack.cmd(req)
@@ -47,7 +30,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  res.send(slack.resp(req, 'Commands: config pass drop relate help'))
+  res.send(slack.resp(req, 'Welcome to baton, an API for easy resource sharing in Slack! Commands: config, pass, drop, relate, help'))
 })
 
 app.get('/v1/batons', (req, res) => {
@@ -78,10 +61,9 @@ app.delete('/v1/batons/:id', (req, res) => {
 
 app.get('/v1/help/:cmd', (req, res) => {
   res.json(slack.resp(req, {
-    pass   : 'Keyword: ```pass```\nExample: ```pass https://api.slack.com/bot-users [slack, bots, api]```',
-    drop   : 'How to drop a baton',
-    relate : 'How to relate tags',
-    search : 'How to search by tag',
+    pass   : 'Create (pass) a baton: ```pass https://api.slack.com/bot-users [slack, bots, api]```',
+    drop   : 'Delete (drop) a baton: ```drop [id|label|url]```',
+    find   : 'Browse batons by tags: ```batons [label|tag(s)]```',
     error  : 'Command must be specified'
   }[req.params.cmd || 'error'] || 'Unsupported command'))
 })
